e440b19fd0ef41ac8abd731948aeffec5ccb58c3
DIEGO DIAS <127305708+d2py@users.noreply.github.com>
2026-03-17 13:43:45 -0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+students = {
+    "Alice": 89.5,
+    "Bob": 76.0,
+     "Charlie": 92.3,
+     "Diana": 84.7,
+    "Ethan": 88.9,
+ }
+
+for nomes, valores in students.items():
+    print(nomes, "->", valores)
+
